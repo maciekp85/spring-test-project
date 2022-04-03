46c1dc2fcf5201fd53fc57d381311460ec357d0f
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2022-04-03 15:25:40 +0200

    XML configuration for mydocuments app

---
+++ r03/src/test/java/com/apress/isf/java/test/MyDocumentsTest.java
@@ -0,0 +1,44 @@
+package com.apress.isf.java.test;
+
+import com.apress.isf.java.model.Document;
+import com.apress.isf.java.model.Type;
+import com.apress.isf.java.service.SearchEngine;
+import com.apress.isf.spring.service.SearchEngineService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyDocumentsTest {
+
+    private ClassPathXmlApplicationContext context;
+    private SearchEngine engine;
+    private Type webtype;
+
+    @Before
+    public void setup() {
+        context = new ClassPathXmlApplicationContext("META-INF/spring/mydocuments-context.xml");
+        engine = context.getBean(SearchEngineService.class);
+        webtype = context.getBean("webType", Type.class);
+    }
+
+    @Test
+    public void testFindByType() {
+        List<Document> result = engine.findByType(webtype);
+        assertNotNull(result);
+        assertTrue(result.size() == 1);
+        assertEquals(webtype.getName(), result.get(0).getType().getName());
+        assertEquals(webtype.getDesc(), result.get(0).getType().getDesc());
+        assertEquals(webtype.getExtension(), result.get(0).getType().getExtension());
+    }
+
+    @Test
+    public void testListAll() {
+        List<Document> result = engine.listAll();
+        assertNotNull(result);
+        assertTrue(result.size() == 4);
+    }
+}
